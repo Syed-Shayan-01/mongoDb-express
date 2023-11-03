d120ecf419146e285056d47e9240d99b13fd2c84
Syed Shayan <shayansyed712@gmail.com>
2023-11-03 23:51:00 +0500

    new chnage

--- middlewares/verify.js
+++ middlewares/verify.js
@@ -2,16 +2,15 @@ const jwt = require('jsonwebtoken')
 const secret_key = require('../secret/Key')
 const Verify = (req, res, next) => {
     const token = req.headers.token;
-
     if (token) {
         jwt.verify(token, secret_key, function (err, decoded) {
             if (err) {
                 res.send('Authentication failed');
             }
-            req.body.email = decoded.email
+            console.log(decoded.email)
         });
-        next();
     }
+    next();
 
 }
 module.exports = Verify;
