02d5315a30224406d29041fe21e910969eeac678
Syed Shayan <shayansyed712@gmail.com>
2023-11-04 22:02:48 +0500

    new change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser')
 var app = express()
 const authRouter = require('./routes/auth')
 const indexRouter = require('./routes/')
-const mongoose = require('./models/db/connnectDb')
+const mongoose = require('./models/db/connnectDb');
 const Verify = require('./middlewares/verify')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
