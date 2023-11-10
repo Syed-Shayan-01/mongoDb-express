17efdb3e52b52c512269b055d6467f2578bc1f87
Syed Shayan <shayansyed712@gmail.com>
2023-11-10 22:04:48 +0500

    new change verify

--- middlewares/verify.js
+++ middlewares/verify.js
@@ -1,18 +1,18 @@
-// const jwt = require('jsonwebtoken');
-// const secret_key = require('../secret/Key');
+const jwt = require('jsonwebtoken');
+const secret_key = require('../secret/Key');
 
-// const Verify = (req, res, next) => {
-//     const authcookie = req.cookies.token;
-//     jwt.verify(authcookie, secret_key, (err, data) => {
-//         if (err) {
-//             res.sendStatus(403)
-//         }
-//         else if (data.user) {
-//             req.email = data.user;
-//             next();
-//         }
-//     })
+const Verify = async (req, res, next) => {
+    const authcookie = req.cookies.token;
+     jwt.verify(authcookie, secret_key, (err, data) => {
+        if (err) {
+            res.sendStatus(403)
+        }
+        else if (data.user) {
+            req.user = data.user;
+            next();
+        }
+    })
 
-// }
+}
 
-// module.exports = Verify;
+module.exports = Verify;
