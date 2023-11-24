7a33f35ed75327a8f780b250f37b09a1495ac9be
Syed Shayan <shayansyed712@gmail.com>
2023-11-24 21:04:04 +0500

    new change signup and login

--- models/authSchema.js
+++ models/authSchema.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 const authSchema = mongoose.Schema({
     username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true}
 });
 
 
 const Auth = mongoose.model('Auth', authSchema);
 
-module.exports = { Auth };
+module.exports = {Auth};
